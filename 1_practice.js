73347ad40dcd02315b261b10bb7ae17587dcf823
arfan36 <arfanur36@gmail.com>
2022-09-15 02:29:37 +0600

    42_5-1 Debug and devtool practice and JavaScript checklist

---
+++ 1_practice.js
@@ -0,0 +1,16 @@
+/* 
+// ─── Revise Javascript ──────────────────────────────────────────────────────────
+1. 6 core concepts: variable, conditional, array, function, loop(for, while,for of, for in), object
+2. arrow function
+3 template String
+4. spread operator
+5. map, filter, forEach, find
+6. destructuring, optional chaining
+7. ternary operator
+8. JSON (parse, stringify), fetch, async await, error
+9. localStorage, sessionStorage
+10. function parameter
+11. dev tool
+12. &&, || , ++, --, !, !!, ===, null, undefined
+ */
+
